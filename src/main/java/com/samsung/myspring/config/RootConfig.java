6218e08db5b705ea260b09abc8434c8ff1ba0490
nisroeld99 <nisroeld99@gmail.com>
2020-03-17 19:48:54 +0900

    first commit

---
+++ src/main/java/com/samsung/myspring/config/RootConfig.java
@@ -0,0 +1,7 @@
+package com.samsung.myspring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+}
---
+++ src/main/java/com/samsung/myspring/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.samsung.myspring.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+}
